963526933ab6107c512d58385516acac0780e875
willculkin <wicu6711@colorado.edu>
2024-09-11 11:41:45 -0400

    adding basic routes

--- base_template/src/App.tsx
+++ base_template/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route, Routes,
+} from 'react-router-dom';
 import SignIn from "./components/SignIn";
+import ForgotPassword from "./components/ForgotPassword";
+import CreateUser from "./components/CreateUser";
+import * as ROUTES from './constants/routes';
+import {CREATE_USER, FORGOT_PASSWORD} from "./constants/routes";
 
 function App() {
   return (
-    <>
-      <SignIn />
-    </>
+    <Router>
+        <Routes>
+            <Route  path={ROUTES.LANDING} element={<SignIn/>} />
+            <Route  path={ROUTES.CREATE_USER} element={<CreateUser/>} />
+            <Route  path={ROUTES.SIGN_IN} element={<SignIn/>} />
+            <Route  path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword/>} />
+        </Routes>
+    </Router>
   );
 }
 
