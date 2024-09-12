b272bd9545822b21a20c89125d8acb4646e021ef
willculkin <wicu6711@colorado.edu>
2024-09-12 13:35:35 -0400

    adding a api call to create user

--- base_template/src/api/CallUsers.ts
+++ base_template/src/api/CallUsers.ts
@@ -1,29 +1,30 @@
-export const makeCall = (email: string, password: string) => {
 let functionUrl = "https://helloworld-c2aaywm6rq-uc.a.run.app";
 if (window.location.hostname === "localhost") {
   functionUrl = "http://127.0.0.1:5001/globaluser-ef76d/us-central1/helloWorld"
 }
-// Email and password to send
+
+export const createUser = async(email: string, password: string, userName: string) => {
 const data = {
-  email,
-  password
+    email,
+    password,
+    userName,
+    eventType: "createUser"
+};
+try {
+    const response = await fetch(functionUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    const result = await response.text();
+    console.log('Response from Cloud Function:', result);
+    return result;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
 };
 
-// Make the HTTP POST request
-fetch(functionUrl, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-  .then(response => response.text())
-  .then(result => {
-      console.log('Response from Cloud Function:', result);
-
-      return(result)
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-}
