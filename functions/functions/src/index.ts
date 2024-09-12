2fb040c811c06dc7b14ccc77b27712bcd6506fc0
willculkin <wicu6711@colorado.edu>
2024-09-12 09:09:00 -0400

    adding cors true to allow cross origin

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -13,7 +13,7 @@ import * as logger from "firebase-functions/logger";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+export const helloWorld = onRequest({cors: true}, (request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
