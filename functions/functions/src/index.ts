58daf3fe755db216b0bc0465d29c9f49d59ed37d
willculkin <wicu6711@colorado.edu>
2024-09-12 13:41:04 -0400

    update function to create a new user

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -9,11 +9,36 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
+import {Firebase} from "./Firebase";
+
+admin.initializeApp();
+const firebase = new Firebase();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest({cors: true}, (request, response) => {
+export const helloWorld = onRequest({cors: true}, async (request, response) => {
   logger.info("Hello logs!", {structuredData: true});
+  await createUser(request.body);
   response.send("Hello from Firebase!");
 });
+
+async function createUser(params: { email: string, password: string, name: string }) {
+  const {email, password, name} = params;
+  const userRecord = await firebase.auth.createUser({
+    email: email,
+    password: password,
+  });
+  const userData = {
+    id: userRecord.uid,
+    email,
+    name,
+  };
+  const returnData = {
+    userData: userData,
+    userRecord,
+  };
+  await firebase.setFireStoreDoc("users", userRecord.uid, userData);
+  return returnData;
+}
